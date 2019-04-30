18d4ff5ca252328e579b38a542a22ff23e3544ea
Vince <git@darkain.com>
2019-04-30 14:33:18 -0700

    More testing

--- test.php
+++ test.php
@@ -1,6 +1,30 @@
 <?php
 
 
+
+
+//ALL WARNINGS AS EXCEPTIONS
+error_reporting(E_ALL);
+set_error_handler(function ($errno, $errstr, $errfile, $errline ) {
+	throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+});
+
+
+//PHP REQUIRES DEFAULT TIMEZONE TO BE SET NOW
+date_default_timezone_set('UTC');
+
+
+//SET FLOATING POINT SERIALIZATION PRECISION TO A KNOWN VALUE
+ini_set('precision', 14);
+ini_set('serialize_precision', 14);
+
+
+//CHANGE TO THIS DIRECTORY FOR CONSISTENCY
+chdir(__DIR__);
+
+
+
+
 $out = [
 
 	http_build_query(
